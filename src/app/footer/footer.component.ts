2f7ea7dba2c11fa9f5290223bf73a90f57e89a7a
Kate Mashokha <katyamashokha@yahoo.com>
2017-10-02 20:33:19 -0700

    Add a flexbox for footer and email verification

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  public userEmail;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onKeyup(event) {
+    this.userEmail = event.target.value;
+    if (this.userEmail === '' || this.userEmail.indexOf('@') === -1 ||  this.userEmail.indexOf('.') === -1) {
+      document.getElementById("errorMessage").innerHTML = "Enter the valid email";
+    } else {
+      document.getElementById("errorMessage").innerHTML = "";
+    }
+}
+
+  submitEmail() {
+  }
 }
