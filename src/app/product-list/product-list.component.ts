5eea5b4c6dc860e954e11429ddf4025995c713f4
ej <evanrjohnso@gmail.com>
2017-10-03 21:53:48 -0700

    add product scrolling functionality

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -15,14 +15,16 @@ import { FirebaseListObservable } from 'angularfire2/database';
 export class ProductListComponent implements OnInit {
   category: string;
   products: any[];
+  // nextButton: boolean = true;
+  backButton: boolean = false;
+  num1: number = 0;
+  num2: number = 11;
 
   constructor(private currentRoute: ActivatedRoute,
               private productService: ProductService,
               private myRouter: Router
             ) { }
-
   ngOnInit() {
-
     this.productService.getProducts().subscribe( lastData => {
       this.products = lastData;
     });
@@ -33,5 +35,17 @@ export class ProductListComponent implements OnInit {
   }
   productWasClicked(clickedProduct) {
     this.myRouter.navigate(['products/item', clickedProduct.Handle]);
+  }
+  nextPage() {
+    this.cycleProducts(12);
+    this.backButton = true;
+  }
+  backPage() {
+    this.cycleProducts(-12);
+  }
+  cycleProducts(input) {
+    this.num1 += input;
+    this.num2 += input;
+    if (this.num1 === 0) this.backButton = false;
   }
 }
