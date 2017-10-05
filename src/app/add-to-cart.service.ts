0424a2c202eb9dd85ec94b0845b6214e9b244dca
ej <evanrjohnso@gmail.com>
2017-10-04 20:20:42 -0700

    implement methods and debug Cart component

--- src/app/add-to-cart.service.ts
+++ src/app/add-to-cart.service.ts
@@ -9,7 +9,6 @@ export class AddToCartService {
     this.myCart = cloudStorage.list('items')
   }
   addItemToCart(itemId: string) {
-
     if (! sessionStorage.getItem('myCart')) {
       sessionStorage.setItem('myCart', itemId);
     } else {
@@ -17,15 +16,18 @@ export class AddToCartService {
                                       .concat("," + itemId);
       sessionStorage.setItem('myCart', updatedCart);
     }
-
   }
-
   findItemInCartById(itemId: string) {
     return this.cloudStorage.object(itemId);
   }
 
   removeItemFromCart(itemId: string) {
-    this.findItemInCartById(itemId).remove();
+    let updatedCart = sessionStorage.getItem('myCart')
+                  .split(',')
+                  .filter(e => e != itemId)
+                  .join(',');
+    if(!updatedCart) return sessionStorage.clear();
+    sessionStorage.setItem('myCart', updatedCart);
   }
 
   getCart() {
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,39 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { AddToCartService }  from '../add-to-cart.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { ProductService } from '../product.service';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
-  providers: [AddToCartService, ProductService]
+  providers: [ AddToCartService, ProductService ]
 })
 export class CartComponent implements OnInit {
    objectsArray: any[] = [];
-  
-
+   myObservable: FirebaseListObservable<any>;
+   subTotal: number;
 
   constructor(private addToCartService: AddToCartService,
               private productService: ProductService) {
-
    }
 
   ngOnInit() {
-    this.addToCartService
+    if (!this.addToCartService.getCart()) {
+      this.subTotal = null;
+    } else {
+      this.addToCartService
         .getCart()
         .split(',')
         .map(item => { this.productService
         .getProductById(item)
-        .subscribe(dataLastEmitted => { this.objectsArray.push(dataLastEmitted);
+        .forEach(dataLastEmitted => {
+          this.objectsArray.push(dataLastEmitted)
+          this.getCumulativeSubTotal();
         })
-        });
-        console.log(this.doIt());
-
+      });
+    }
   }
-  doIt() {
-    return this.objectsArray.reduce( function(accumulator, current){
+
+  getCumulativeSubTotal() {
+    this.subTotal = this.objectsArray.reduce(
+                          function(accumulator, current){
                           return accumulator + current.Variant_Price
-                        }, 0);
+                              }, 0);
+}
+
+removeButtonWasClicked(payload) {
+  this.addToCartService.removeItemFromCart(payload.$key);
 }
 
 
